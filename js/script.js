e910fbeeef257223865b64d9f4463b8fd7a51c4d
giuliamari04 <giuliamari04@gmail.com>
2023-10-11 15:01:06 +0200

    codice migliorato

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 const wrapperEl = document.getElementById('boxWrapper');
 
-for(let i = 1; i<=100; i++){
-    if(i % 3 ===0){
-        console.log('Fizz');
-       
-    }else if( i% 5 ===0 ){
-       console.log('Buzz');
-      
-    }else{
-        console.log(i);
-       
+for (let i = 1; i <= 100; i++) {
+    let numero = '';
+    
+    if (i % 3 === 0){  
+        numero += 'Fizz'; 
+     } 
+    if (i % 5 === 0){ 
+        numero += 'Buzz';
     }
-   
+    if (numero === '') {
+        numero = i;
+    }
+
+    console.log(numero);
+
+    const boxEl = document.createElement('div');
+    boxEl.className = `box col-2 p-3`;
+    boxEl.innerHTML = numero;
+    wrapperEl.append(boxEl);
 }
