d3b1e2f6b8449588699dbe1380bd7521783fd853
giuliamari04 <giuliamari04@gmail.com>
2023-10-11 15:13:25 +0200

    la vecchia strada è la più sicura

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@ const wrapperEl = document.getElementById('boxWrapper');
 
 for (let i = 1; i <= 100; i++) {
     let numero = '';
+    let bgBox;
     
-    if (i % 3 === 0){  
-        numero += 'Fizz'; 
-     } 
-    if (i % 5 === 0){ 
-        numero += 'Buzz';
+    if (i % 3 === 0 && i % 5 === 0 ){  
+        numero = 'FizzBuzz'; 
+        bgBox = 'my-bg-fizzbuzz';
+
+     } else if (i % 3 === 0){  
+        numero = 'Fizz'; 
+        bgBox = 'my-bg-fizz';
+
+     } else if (i % 5 === 0){ 
+        numero = 'Buzz';
+        bgBox = 'my-bg-buzz';
     }
     if (numero === '') {
         numero = i;
@@ -16,7 +23,7 @@ for (let i = 1; i <= 100; i++) {
     console.log(numero);
 
     const boxEl = document.createElement('div');
-    boxEl.className = `box col-2 p-3`;
+    boxEl.className = `box ${bgBox} col-2 p-3`;
     boxEl.innerHTML = numero;
     wrapperEl.append(boxEl);
 }
